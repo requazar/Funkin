0c360739e10186895f76463960ec50b95c28e87a
requazar <spinklebop87@gmail.com>
2026-05-16 21:10:53 -0700

    Fix PlayStatePlaylist using incorrect script variations

--- source/funkin/play/PlayState.hx
+++ source/funkin/play/PlayState.hx
@@ -3670,7 +3670,8 @@ class PlayState extends MusicBeatSubState
         }
         else
         {
-          var targetSong:Song = SongRegistry.instance.fetchEntry(targetSongId) ?? throw 'Could not find a song with ID $targetSongId';
+          var targetSong:Song = SongRegistry.instance.fetchEntry(targetSongId,
+            {variation: currentVariation}) ?? throw 'Could not find a song with ID $targetSongId';
           var targetVariation:String = currentVariation;
           if (!targetSong.hasDifficulty(PlayStatePlaylist.campaignDifficulty, currentVariation))
           {
